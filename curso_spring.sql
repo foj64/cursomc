a64c214de2a1f73b76155323e402f9db3ba76ad0
Filipe Oliveira João <filipe@sst.sc.gov.br>
2019-03-25 19:04:17 -0300

    7

---
+++ curso_spring.sql
@@ -0,0 +1,349 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 25-Mar-2019 às 22:37
+-- Versão do servidor: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `curso_spring`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(1, 'Informática'),
+(2, 'Escritório'),
+(3, 'Cama mesa e banho'),
+(4, 'Eletrônicos'),
+(5, 'Jardinagem'),
+(6, 'Decoração'),
+(7, 'Perfumaria');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cidade`
+--
+
+CREATE TABLE IF NOT EXISTS `cidade` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `estado_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKkworrwk40xj58kevvh3evi500` (`estado_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `cidade`
+--
+
+INSERT INTO `cidade` (`id`, `nome`, `estado_id`) VALUES
+(1, 'Uberlândia', 1),
+(2, 'São Paulo', 2),
+(3, 'Campinas', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cpf_ou_cnpj` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_cmxo70m08n43599l3h0h07cc6` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `cpf_ou_cnpj`, `email`, `nome`, `senha`, `tipo`) VALUES
+(1, '36378912377', 'maria@gmail.com', 'Maria Silva', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+CREATE TABLE IF NOT EXISTS `endereco` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cep` varchar(255) DEFAULT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
+  `numero` varchar(255) DEFAULT NULL,
+  `cidade_id` int(11) DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK8b1kcb3wucapb8dejshyn5fsx` (`cidade_id`),
+  KEY `FK8s7ivtl4foyhrfam9xqom73n9` (`cliente_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `endereco`
+--
+
+INSERT INTO `endereco` (`id`, `bairro`, `cep`, `complemento`, `logradouro`, `numero`, `cidade_id`, `cliente_id`) VALUES
+(1, 'Jardim', '38220834', 'Apto 303', 'Rua Flores', '300', 1, 1),
+(2, 'Centro', '38777012', 'Sala 800', 'Avenida Matos', '105', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estado`
+--
+
+CREATE TABLE IF NOT EXISTS `estado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `estado`
+--
+
+INSERT INTO `estado` (`id`, `nome`) VALUES
+(1, 'Minas Gerais'),
+(2, 'São Paulo');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `item_pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `item_pedido` (
+  `desconto` double DEFAULT NULL,
+  `preco` double DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL,
+  PRIMARY KEY (`pedido_id`,`produto_id`),
+  KEY `FKtk55mn6d6bvl5h0no5uagi3sf` (`produto_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `item_pedido`
+--
+
+INSERT INTO `item_pedido` (`desconto`, `preco`, `quantidade`, `pedido_id`, `produto_id`) VALUES
+(0, 2000, 1, 1, 1),
+(0, 80, 2, 1, 3),
+(100, 800, 1, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento`
+--
+
+CREATE TABLE IF NOT EXISTS `pagamento` (
+  `pedido_id` int(11) NOT NULL,
+  `estado` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pedido_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pagamento`
+--
+
+INSERT INTO `pagamento` (`pedido_id`, `estado`) VALUES
+(1, 2),
+(2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento_com_boleto`
+--
+
+CREATE TABLE IF NOT EXISTS `pagamento_com_boleto` (
+  `data_pagamento` datetime DEFAULT NULL,
+  `data_vencimento` datetime DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL,
+  PRIMARY KEY (`pedido_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pagamento_com_boleto`
+--
+
+INSERT INTO `pagamento_com_boleto` (`data_pagamento`, `data_vencimento`, `pedido_id`) VALUES
+(NULL, '2017-10-20 02:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento_com_cartao`
+--
+
+CREATE TABLE IF NOT EXISTS `pagamento_com_cartao` (
+  `numero_de_parcelas` int(11) DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL,
+  PRIMARY KEY (`pedido_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `pagamento_com_cartao`
+--
+
+INSERT INTO `pagamento_com_cartao` (`numero_de_parcelas`, `pedido_id`) VALUES
+(6, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE IF NOT EXISTS `pedido` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `instante` datetime DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `endereco_de_entrega_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK30s8j2ktpay6of18lbyqn3632` (`cliente_id`),
+  KEY `FK1fihyy2fnocpuwc74674qmfkv` (`endereco_de_entrega_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Extraindo dados da tabela `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `instante`, `cliente_id`, `endereco_de_entrega_id`) VALUES
+(1, '2017-09-30 13:32:00', 1, 1),
+(2, '2017-10-10 22:35:00', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `perfis`
+--
+
+CREATE TABLE IF NOT EXISTS `perfis` (
+  `cliente_id` int(11) NOT NULL,
+  `perfis` int(11) DEFAULT NULL,
+  KEY `FKsobr8hl9guwr8775lyl1mncg2` (`cliente_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE IF NOT EXISTS `produto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) DEFAULT NULL,
+  `preco` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `preco`) VALUES
+(1, 'Computador', 2000),
+(2, 'Impressora', 800),
+(3, 'Mouse', 80),
+(4, 'Mesa de escritório', 300),
+(5, 'Toalha', 50),
+(6, 'Colcha', 200),
+(7, 'TV true color', 1200),
+(8, 'Roçadeira', 800),
+(9, 'Abajour', 100),
+(10, 'Pendente', 180),
+(11, 'Shampoo', 90);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto_categoria`
+--
+
+CREATE TABLE IF NOT EXISTS `produto_categoria` (
+  `produto_id` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  KEY `FKq3g33tp7xk2juh53fbw6y4y57` (`categoria_id`),
+  KEY `FK1c0y58d3n6x3m6euv2j3h64vt` (`produto_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `produto_categoria`
+--
+
+INSERT INTO `produto_categoria` (`produto_id`, `categoria_id`) VALUES
+(1, 1),
+(1, 4),
+(2, 1),
+(2, 2),
+(2, 4),
+(3, 1),
+(3, 4),
+(4, 2),
+(5, 3),
+(6, 3),
+(7, 4),
+(8, 5),
+(9, 6),
+(10, 6),
+(11, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `telefone`
+--
+
+CREATE TABLE IF NOT EXISTS `telefone` (
+  `cliente_id` int(11) NOT NULL,
+  `telefones` varchar(255) DEFAULT NULL,
+  KEY `FK8aafha0njkoyoe3kvrwsy3g8u` (`cliente_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `telefone`
+--
+
+INSERT INTO `telefone` (`cliente_id`, `telefones`) VALUES
+(1, '27363323'),
+(1, '93838393');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/main/java/com/foj/cursomc/service/PedidoService.java
+++ src/main/java/com/foj/cursomc/service/PedidoService.java
@@ -1,11 +1,19 @@
 package com.foj.cursomc.service;
 
+import java.util.Date;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.foj.cursomc.domain.ItemPedido;
+import com.foj.cursomc.domain.PagamentoComBoleto;
 import com.foj.cursomc.domain.Pedido;
+import com.foj.cursomc.domain.enums.EstadoPagamento;
+import com.foj.cursomc.repositories.ItemPedidoRepository;
+import com.foj.cursomc.repositories.PagamentoRepository;
 import com.foj.cursomc.repositories.PedidoRepository;
 import com.foj.cursomc.service.exceptions.ObjectNotFoundException;
 
@@ -15,10 +23,43 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 
 }
